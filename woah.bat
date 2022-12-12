d6f5d25a7b6f2a72c5d2b7ba6cce0c4e53a9e08d
JustAFriendlyCat <41790044+JustAFriendlyCat@users.noreply.github.com>
2022-12-12 18:40:31 +0100

    a

--- woah.bat
+++ woah.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk --private-window https://www.youtube.com/embed/8GEKjBEm0SA?controls=0
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk --private-window https://www.youtube.com/embed/8GEKjBEm0SA?autoplay=1&controls=0
 
 
 

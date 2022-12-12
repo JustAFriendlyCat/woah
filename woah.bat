3c04ef9c241f0f9dbed76d90bea5c6d4f16b1107
JustAFriendlyCat <41790044+JustAFriendlyCat@users.noreply.github.com>
2022-12-12 18:36:37 +0100

    a

--- woah.bat
+++ woah.bat
@@ -1,3 +1,4 @@
 "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk --private-window https://www.youtube.com/embed/8GEKjBEm0SA?controls=0
 
-pnputil /disable-device /deviceid "USB\Class_03"
+
+
